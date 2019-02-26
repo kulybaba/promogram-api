ee0f4e825f2c9d659ebece55e457a0521bc43090
Petro Kulybaba <ahurtep@gmail.com>
2019-02-26 13:35:41 +0200

    Registration

---
+++ src/Controller/UserController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Company;
+use App\Entity\User;
+use App\Services\EmailService;
+use App\Services\UserService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+/**
+ * @Route("/api")
+ */
+class UserController extends AbstractController
+{
+    /**
+     * @var SerializerInterface
+     */
+    private $serializer;
+
+    /**
+     * @var ValidatorInterface
+     */
+    private $validator;
+
+    /**
+     * @var UserService
+     */
+    private $userService;
+
+    /**
+     * @var EmailService
+     */
+    private $emailService;
+
+    public function __construct(SerializerInterface $serializer, ValidatorInterface $validator, UserService $userService,EmailService $emailService)
+    {
+        $this->serializer = $serializer;
+        $this->validator = $validator;
+        $this->userService = $userService;
+        $this->emailService = $emailService;
+    }
+
+    /**
+     * @Route("/registration", methods={"POST"})
+     */
+    public function registrationAction(Request $request)
+    {
+        if (!$request->getContent()) {
+            throw new HttpException('400', 'Bad request');
+        }
+
+        $user = $this->serializer->deserialize($request->getContent(), User::class, JsonEncoder::FORMAT);
+        $user->setPassword($this->userService->encodePassword($user));
+        $user->setApiToken($this->userService->generateApiToken());
+        $user->setApproved(false);
+
+        $company = $this->serializer->deserialize($request->getContent(), Company::class, JsonEncoder::FORMAT);
+        $company->setUser($user);
+
+        if (count($this->validator->validate($company)) || count($this->validator->validate($user))) {
+            throw new HttpException('400', 'Bad request');
+        }
+
+        $em = $this->getDoctrine()->getManager();
+        $em->persist($company);
+        $em->flush();
+
+        $this->emailService->sendRegistrationEmail($user);
+
+        return $this->json($user);
+    }
+}
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,12 +5,15 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @UniqueEntity(fields="email", message="Email already taken")
  */
-class User implements UserInterface
+class User implements UserInterface, \JsonSerializable
 {
     /**
      * @ORM\Id()
@@ -20,6 +23,12 @@ class User implements UserInterface
     private $id;
 
     /**
+     * @Assert\NotBlank()
+     * @Assert\Email(
+     *     message="The email '{{ value }}' is not a valid email.",
+     *     checkMX = true
+     * )
+     * @var string
      * @ORM\Column(type="string", length=180, unique=true)
      */
     private $email;
@@ -36,21 +45,44 @@ class User implements UserInterface
     private $password;
 
     /**
+     * @Assert\NotBlank()
+     * @Assert\Length(
+     *     max="20",
+     *     min="2",
+     *     maxMessage="First name must contain maximum 20 characters.",
+     *     minMessage="First name must contain minimum 2 characters."
+     * )
+     * @var string
      * @ORM\Column(type="string", length=255)
      */
     private $firstName;
 
     /**
+     * @Assert\NotBlank()
+     * @Assert\Length(
+     *     max="20",
+     *     min="2",
+     *     maxMessage="Last name must contain maximum 20 characters.",
+     *     minMessage="Last name must contain minimum 2 characters."
+     * )
+     * @var string
      * @ORM\Column(type="string", length=255)
      */
     private $lastName;
 
     /**
+     * @Assert\Length(
+     *     max="255",
+     *     maxMessage="About must contain maximum 255 characters."
+     * )
+     * @var string
      * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $about;
 
     /**
+     * @Assert\NotBlank()
+     * @var string
      * @ORM\Column(type="string", length=255)
      */
     private $picture;
@@ -105,8 +137,21 @@ class User implements UserInterface
      */
     private $likes;
 
+    /**
+     * @Assert\NotBlank()
+     * @Assert\Length(
+     *     max="30",
+     *     min="6",
+     *     maxMessage="Password must contain maximum 30 characters.",
+     *     minMessage="Password must contain minimum 6 characters."
+     * )
+     * @var string
+     */
+    private $plainPassword;
+
     public function __construct()
     {
+        $this->roles = ['ROLE_RETAILER'];
         $this->company = new ArrayCollection();
         $this->following = new ArrayCollection();
         $this->followers = new ArrayCollection();
@@ -150,8 +195,6 @@ class User implements UserInterface
     public function getRoles(): array
     {
         $roles = $this->roles;
-        // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_USER';
 
         return array_unique($roles);
     }
@@ -505,5 +548,33 @@ class User implements UserInterface
         }
 
         return $this;
+    }
+
+    public function getPlainPassword(): ?string
+    {
+        return $this->plainPassword;
+    }
+
+    public function setPlainPassword(string $plainPassword): self
+    {
+        $this->plainPassword = $plainPassword;
+
+        return $this;
+    }
+
+    public function jsonSerialize()
+    {
+        return [
+            'id' => $this->getId(),
+            'approved' => $this->getApproved(),
+            'firstName' => $this->getFirstName(),
+            'lastName' => $this->getLastName(),
+            'about' => $this->getAbout(),
+            'picture' => $this->getPicture(),
+            'role' => $this->getRoles(),
+            'email' => $this->getEmail(),
+            'password' => $this->getPassword(),
+            'api_token' => $this->getApiToken()
+        ];
     }
 }
