2df4693ee9d27eae6d15359b318d3be3dd940b95
Petro Kulybaba <ahurtep@gmail.com>
2019-03-29 11:44:37 +0200

    Moved followAction and unfollowAction from FollowController to ProfileController

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -151,6 +151,58 @@ class ProfileController extends AbstractController
     }
 
     /**
+     * @Route("/profile/{id}/follow", requirements={"id"="\d+"}, methods={"POST"})
+     */
+    public function followAction(User $followingUser)
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        $this->denyAccessUnlessGranted('follow', $followingUser);
+
+        /** @var User $user */
+        $user = $this->getUser();
+
+        if ($this->em->getRepository(User::class)->findFollow($user->getId(), $followingUser->getId())) {
+            throw new HttpException(Response::HTTP_FORBIDDEN, 'You are already followed');
+        }
+
+        $user->addFollowing($followingUser);
+
+        $this->em->flush();
+
+        return $this->json([
+            'success' => true,
+            'message' => 'You are successful followed',
+        ]);
+    }
+
+    /**
+     * @Route("/profile/{id}/unfollow", requirements={"id"="\d+"}, methods={"DELETE"})
+     */
+    public function unfollowAction(User $unfollowingUser)
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        $this->denyAccessUnlessGranted('unfollow', $unfollowingUser);
+
+        /** @var User $user */
+        $user = $this->getUser();
+
+        if (!$this->em->getRepository(User::class)->findFollow($user->getId(), $unfollowingUser->getId())) {
+            throw new HttpException(Response::HTTP_FORBIDDEN, "You don't follow on this user");
+        }
+
+        $user->removeFollowing($unfollowingUser);
+
+        $this->em->flush();
+
+        return $this->json([
+            'success' => true,
+            'message' => 'You are successful unfollowed',
+        ]);
+    }
+
+    /**
      * @Route("/profile/{id}/followers", requirements={"id"="\d+"}, methods={"GET"})
      */
     public function followersAction(User $user)
